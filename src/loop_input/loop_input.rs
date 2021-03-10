c3bcf0774793bc8291ce85056ec1212760b1acec
shinyaigeek <PhilisPaxil@gmail.com>
2021-03-10 20:30:47 +0900

    Feature: repl

--- src/loop_input/loop_input.rs
+++ src/loop_input/loop_input.rs
@@ -1,6 +1,12 @@
+use std::io::{self, Read};
+
 pub fn loop_input<CF>(mut handle_input: CF)
 where
     CF: FnMut(String),
 {
-    handle_input("10;".to_string())
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer);
+    handle_input(buffer);
+
+    loop_input(handle_input);
 }
