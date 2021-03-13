5dbcff5744aff99491b2f5582d661f0cf2088a4e
shinyaigeek <PhilisPaxil@gmail.com>
2021-03-13 17:21:59 +0900

    Feature: split expression from handle_expression

--- src/evaluator/evaluator.rs
+++ src/evaluator/evaluator.rs
@@ -1,42 +1,18 @@
 use crate::evaluator::object::object::{Bool, Integer, Object};
-use crate::parser::parser::{Parser, Expression, Literal, LiteralType, Node, Statement, StatementType};
 use crate::parser::lexer::lexer::Lexer;
-use crate::parser::tokenizer::tokenizer::{Tokens};
+use crate::parser::parser::{
+    Expression, Literal, LiteralType, Node, Parser, Statement, StatementType,
+};
+use crate::parser::tokenizer::tokenizer::Tokens;
 
 pub fn evaluate(node: Node) -> Object {
     match node {
         Node::Program(program) => evaluate_statements(program.body),
         Node::Statement(statement) => match statement.statement {
-            StatementType::Expression(expression) => match expression {
-                Expression::Literal(literal) => match literal.literal_type {
-                    LiteralType::INT => {
-                        let int: i32 = literal.value.parse().unwrap();
-                        Object::Integer(Integer { value: int })
-                    }
-                    LiteralType::BOOLEAN => {
-                        let bool: bool = literal.value.parse().unwrap();
-                        Object::Bool(Bool { value: bool })
-                    }
-                    _ => panic!(""),
-                },
-                _ => panic!(""),
-            },
+            StatementType::Expression(expression) => handle_expression(expression),
             _ => panic!(""),
         },
-        Node::Expression(expression) => match expression {
-            Expression::Literal(literal) => match literal.literal_type {
-                LiteralType::INT => {
-                    let int: i32 = literal.value.parse().unwrap();
-                    Object::Integer(Integer { value: int })
-                }
-                LiteralType::BOOLEAN => {
-                    let bool: bool = literal.value.parse().unwrap();
-                    Object::Bool(Bool { value: bool })
-                }
-                _ => panic!(""),
-            },
-            _ => panic!(""),
-        },
+        Node::Expression(expression) => handle_expression(expression),
         _ => panic!(""),
     }
 }
@@ -54,6 +30,23 @@ pub fn evaluate_statements(statements: Vec<Statement>) -> Object {
 
 pub fn statement_to_node(statement: Statement) -> Node {
     Node::Statement(statement)
+}
+
+pub fn handle_expression(expression: Expression) -> Object {
+    match expression {
+        Expression::Literal(literal) => match literal.literal_type {
+            LiteralType::INT => {
+                let int: i32 = literal.value.parse().unwrap();
+                Object::Integer(Integer { value: int })
+            }
+            LiteralType::BOOLEAN => {
+                let bool: bool = literal.value.parse().unwrap();
+                Object::Bool(Bool { value: bool })
+            }
+            _ => panic!(""),
+        },
+        _ => panic!(""),
+    }
 }
 
 #[cfg(test)]
