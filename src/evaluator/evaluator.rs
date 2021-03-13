fba3b0f9e8bbcd57d320559712747a62d4942804
shinyaigeek <PhilisPaxil@gmail.com>
2021-03-13 17:56:54 +0900

    Feature: eval PrefixExpression

--- src/evaluator/evaluator.rs
+++ src/evaluator/evaluator.rs
@@ -1,7 +1,7 @@
 use crate::evaluator::object::object::{Bool, Integer, Object};
 use crate::parser::lexer::lexer::Lexer;
 use crate::parser::parser::{
-    Expression, Literal, LiteralType, Node, Parser, Statement, StatementType,
+    Expression, Literal, LiteralType, Node, Parser, PrefixOperator, Statement, StatementType,
 };
 use crate::parser::tokenizer::tokenizer::Tokens;
 
@@ -9,10 +9,10 @@ pub fn evaluate(node: Node) -> Object {
     match node {
         Node::Program(program) => evaluate_statements(program.body),
         Node::Statement(statement) => match statement.statement {
-            StatementType::Expression(expression) => handle_expression(expression),
+            StatementType::Expression(expression) => handle_expression(Box::new(expression)),
             _ => panic!(""),
         },
-        Node::Expression(expression) => handle_expression(expression),
+        Node::Expression(expression) => handle_expression(Box::new(expression)),
         _ => panic!(""),
     }
 }
@@ -32,8 +32,8 @@ pub fn statement_to_node(statement: Statement) -> Node {
     Node::Statement(statement)
 }
 
-pub fn handle_expression(expression: Expression) -> Object {
-    match expression {
+pub fn handle_expression(expression: Box<Expression>) -> Object {
+    match *expression {
         Expression::Literal(literal) => match literal.literal_type {
             LiteralType::INT => {
                 let int: i32 = literal.value.parse().unwrap();
@@ -45,7 +45,39 @@ pub fn handle_expression(expression: Expression) -> Object {
             }
             _ => panic!(""),
         },
+        Expression::PrefixExpression(prefix_expression) => {
+            let obj = handle_expression(prefix_expression.right);
+            match prefix_expression.operator {
+                PrefixOperator::MINUS => {
+                    handle_prefix_literal(obj, PrefixOperator::MINUS)
+                },
+                PrefixOperator::EXCLAMATION => {
+                    handle_prefix_literal(obj, PrefixOperator::EXCLAMATION)
+                },
+                _ => panic!("prefix operator should be - or !"),
+            }
+        }
         _ => panic!(""),
+    }
+}
+
+pub fn handle_prefix_literal(obj: Object, prefix: PrefixOperator) -> Object {
+    if prefix == PrefixOperator::MINUS {
+        match obj {
+            Object::Integer(int) => Object::Integer(Integer {
+                value: -1 * int.value,
+            }),
+            Object::Bool(bool) => Object::Integer(Integer {
+                value: if bool.value { -1 } else { 0 },
+            }),
+        }
+    } else {
+        match obj {
+            Object::Integer(int) => Object::Bool(Bool {
+                value: int.value == 0,
+            }),
+            Object::Bool(bool) => Object::Bool(Bool { value: !bool.value }),
+        }
     }
 }
 
