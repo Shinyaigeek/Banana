835a0ab234f3836ff1d588fe146eea52362aadc2
shinyaigeek <PhilisPaxil@gmail.com>
2021-03-15 23:02:13 +0900

    Feature: multiple input for repl

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,42 @@ mod evaluator;
 mod loop_input;
 mod parser;
 use crate::evaluator::evaluator::evaluate;
+use crate::evaluator::variable::variable::{Environment, VariableValue};
 use crate::loop_input::loop_input::loop_input;
 use crate::parser::lexer::lexer::Lexer;
 use crate::parser::parser::{Node, Parser};
 use crate::parser::tokenizer::tokenizer::{Token, TokenType, Tokens};
 
 fn main() {
-    loop_input(|src| {
-        let mut lexer = Lexer::new(&src);
-        let mut tokens = Tokens::new(lexer);
-        let mut parser = Parser::new(tokens);
-        parser.parse();
-        // let node = Node::Program(parser.program);
-        // let result = evaluate(node);
-        println!("{:?}", parser.program);
-    });
+    let mut environment = Environment::new();
+    loop_input(
+        |src| {
+            let mut lexer = Lexer::new(&src);
+            let mut tokens = Tokens::new(lexer);
+            let mut parser = Parser::new(tokens);
+            parser.parse();
+            let node = Node::Program(parser.program);
+            let result = evaluate(node, &mut environment);
+            println!("{:?}", result.inspect());
+        },
+        "",
+        0,
+    );
+
+    //     let src = String::from(
+    //         "let i = 1;
+    // if (1 == 1) {
+    // let i = 9 * 6;
+    // };
+    // i + 1;",
+    //     );
+
+    //     let mut lexer = Lexer::new(&src);
+    //     let mut tokens = Tokens::new(lexer);
+    //     let mut parser = Parser::new(tokens);
+    //     parser.parse();
+    //     let node = Node::Program(parser.program);
+    //     let mut environment = Environment::new();
+    //     let result = evaluate(node, &mut environment);
+    //     println!("{:?}", result.inspect());
 }
