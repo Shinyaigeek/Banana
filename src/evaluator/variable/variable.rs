860934edc49a391d6dab2c06ae33c2c54064ba2b
shinyaigeek <PhilisPaxil@gmail.com>
2021-03-15 19:23:12 +0900

    Feature: add environment for variable declaration

--- src/evaluator/variable/variable.rs
+++ src/evaluator/variable/variable.rs
@@ -1,6 +1,49 @@
 pub use crate::evaluator::object::object::Object;
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq)]
 pub enum VariableValue {
     Object(Object),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Environment {
+    // * mutableかどうかなどはvalueの方でもつ
+    variables: HashMap<String, VariableValue>,
+    outer: Option<Box<Environment>>,
+}
+
+impl Environment {
+    pub fn get(&self, identifier: String) -> &VariableValue {
+        if self.variables.contains_key(&identifier) {
+            return self.variables.get(&identifier).unwrap();
+        };
+
+        let res = match &self.outer {
+            Some(env) => env.get(identifier),
+            None => panic!("{} is not defined", identifier),
+        };
+
+        res
+    }
+
+    pub fn set(&mut self, identifier: String, value: VariableValue) -> &VariableValue {
+        self.variables.insert(identifier.clone(), value);
+        self.variables.get(&identifier).unwrap()
+    }
+
+    pub fn extend(self) -> Environment {
+        Environment {
+            variables: HashMap::new(),
+            // TODO waste memory
+            outer: Some(Box::new(self)),
+        }
+    }
+
+    pub fn new() -> Environment {
+        Environment {
+            variables: HashMap::new(),
+            outer: None,
+        }
+    }
 }
